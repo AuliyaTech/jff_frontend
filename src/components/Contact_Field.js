cc3102f001013427b170ab98ae86e3420ddc79a2
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2020-03-17 15:57:20 -0400

    modified contact feild

--- src/components/Contact_Field.js
+++ src/components/Contact_Field.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs-com';
 
 export default function ContactUs() {
 
-  function sendEmail(e) {
-    e.preventDefault();
+  // function sendEmail(e) {
+  //   e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  }
+  //   emailjs.sendForm('jjfsquad_gmail_com', 'YOUR_TEMPLATE_ID', e.target, 'user_OELfxfrUXstpXtD3RJaG0')
+  //     .then((result) => {
+  //         console.log(result.text);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+  // }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
+    <form className="contact-form" /*onSubmit={sendEmail}*/>
       <input type="hidden" name="contact_number" />
       <label>Name</label>
       <input type="text" name="user_name" />
