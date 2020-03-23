98c9145d9812a4d5de8fdebf71a228166820462d
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2020-03-23 15:24:25 -0400

    fixed contact feild

--- src/components/Contact_Field.js
+++ src/components/Contact_Field.js
@@ -1,30 +1,80 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button"
+import {Container, Row, Col} from "./Grid"
 import emailjs from 'emailjs-com';
 
-export default function ContactUs() {
+class ContactUs extends Component {
+  constructor(props){
+    super(props);
 
-  function sendEmail(e) {
-    e.preventDefault();
-    console.log(e.target)
+    this.state={
+        status: null
+    }
+}
+
+  sendEmail(e) {
+    e.preventDefault()
 
     emailjs.sendForm('jjfsquad_gmail_com', 'jiajamfit', e.target, 'user_OELfxfrUXstpXtD3RJaG0')
       .then((result) => {
-          console.log(result.text);
+          this.setState({ status: 200 })
       }, (error) => {
           console.log(error.text);
       });
+
+      // this.setState({status: x})
+  }
+  render(){
+    return (
+      <div className="contact-form">
+        <Container>
+          <Row>
+            <h1 className="page-title">We'd Love to Hear from You..</h1>
+          </Row>
+
+          <Form onSubmit={() => this.sendEmail()}>
+
+            <Form.Group as={Row} controlId="formPlaintextFName">
+              <Form.Label column md={2} xs={12}>
+                <div className="know-input">First Name:</div>
+              </Form.Label>
+              <Col size="8">
+                <Form.Control name="user_name"/>
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Form.Label column md={2} xs={12}>
+              <div className="know-input">Email:</div>
+              </Form.Label>
+              <Col size="8">
+                <Form.Control name="user_email"/>
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              <Form.Label column md={2} xs={12}>
+              <div className="know-input">Message:</div>
+              </Form.Label>
+              <Col size="8">
+                <Form.Control as="textarea" rows={3} name="message"/>
+              </Col>
+            </Form.Group>
+
+            <Button variant="light" type="submit" className="float-right mr-5">
+              Submit
+            </Button> 
+            
+          </Form>
+        </Container>
+      
+      </div>
+    );
   }
 
-  return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
 }
+ 
+// }
+
+export default ContactUs;
